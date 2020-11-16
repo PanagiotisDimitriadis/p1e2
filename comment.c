30afdfd891c91be2e5c5e06d5f102663c2aae9f5
unknown <Panos.dim125@hotmail.com>
2020-11-17 01:49:48 +0200

    comment

---
+++ comment.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main (void)
+{
+	printf("den katalaba ti tha xreiastei na kanoume!\n");
+	return 0;
+}
---
+++ oneline.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+	
+	printf("Panagiotis \n ILLUMINATI");
+	return 0;
+}
